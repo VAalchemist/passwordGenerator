973a98563ceca20b78623772dc612dd4d33edaf3
CharK <cbkayee91@gmail.com>
2022-01-16 23:06:21 -0800

    random number generator

--- Develope/assets/script/script.js
+++ Develope/assets/script/script.js
@@ -4,12 +4,19 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min) + min);
+
+    return value;
+};
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword("#generate");
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
